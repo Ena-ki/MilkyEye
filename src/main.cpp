34d3b4bb6286c2b59c99cf30009c2fda994e8bc2
Ena <teankim2@gmail.com>
2026-03-06 20:28:54 +0900

    link GLAD and add a basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include <algorithm>
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main () {
-  std::cout << "hello world";
+
+int main(void)
+{
+  GLFWwindow* window;
+  if (!glfwInit())
+    return -1;
+
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+    return -1;
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+      return -1;
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  return 0;
 }
